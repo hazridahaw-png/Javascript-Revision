c77f5d7c5a487023f50000d6561c5abd13a26890
hazridahaw-png <hazridahaw@gmail.com>
2025-12-02 22:31:19 +0000

    Added Rida

--- contact.js
+++ contact.js
@@ -1,1 +1,22 @@
-c
+let contactDatabase = [
+
+    {
+        "name": "Rida",
+        "surname": "Aw",
+        "email": "ridahaw@gmail.com",
+        "favorite": true,
+        "priority": 1
+    }
+]
+function displayContactList() {
+
+}
+function AddNewContact (contactDatabase, name,surname,email,favorite,priority){
+let Newcontact= {
+"name":"name",
+"surname":"surname",
+"email":"email",
+"favorite":"favorite",
+"priority":"priority"}
+contactDatabase.push(Newcontact);
+}
