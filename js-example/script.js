5accab791a50c6235d80f28dd85e0a7c4aa417e3
hazridahaw-png <hazridahaw@gmail.com>
2025-12-04 06:21:53 +0000

    Added change

--- js-example/script.js
+++ js-example/script.js
@@ -1,31 +1,51 @@
-console.log("hello world");
-let s = "hi kitty cat";
+let plusButton = document.querySelector("#plusButton");
+plusButton.addEventListener("click", function () {
+    // when working with DOM, we SELECT, then we MODIFY
+    let numberBox = document.querySelector("#number");
 
-let btn = document.querySelector("#changeBtn");
-btn.addEventListener("click", function (){
-   
+    // take the current innerText from the number box
+    let currentNumber = parseInt(numberBox.innerText);
+    if (currentNumber < 10) {
+        // increase by 1
+        currentNumber += 1;
+    }
+    // set the innerText to the new current number
+    numberBox.innerText = currentNumber;
+    if (currentNumber == 0) {
+        numberBox.style.color = "black"
+    }
+    else if (currentNumber % 2 == 1) {
+        numberBox.style.color = "red";
+    }
+    else if (currentNumber % 2 == 0) {
+        numberBox.style.color = "green";
+    }
 
-console.log (s.toLocaleUpperCase());
-let heading = document.querySelector("h1");
-heading.style.backgroundColor = "red"
-let heading2 = document.querySelector("h2");
-heading2.style.fontFamily = "Verdana";
+});
 
-let skills = document.querySelector("#skills");
-skills.style.border = "1px solid black";
+let zeroButton = document.querySelector("#zeroButton");
+zeroButton.addEventListener("click", function () {
+    let numberBox = document.querySelector("#number");
+    numberBox.innerText = 0;
+    numberBox.style.color = "black";
+})
 
-let expertSkill = document.querySelector(".expert");
-expertSkill.style.backgroundColor = "yellow";
+let minusButton = document.querySelector("#minusButton");
+minusButton.addEventListener("click", function () {
+    let numberBox = document.querySelector("#number");
+    let currentNumber = numberBox.innerText;
+    if (currentNumber > -10) {
+         currentNumber -= 1;
+    }
+    numberBox.innerText = currentNumber;
 
-let allExpertSkills=document.querySelectorAll("expert");
-console.log(allExpertSkills);
-
-for (let skill of allExpertSkills){
-    skill.style.color = "red";
-}
-
-let link = document.querySelector("#search");
-link.href = "https://www.bing.com";
-link.innerText = "Bing";
-
+    if (currentNumber == 0) {
+        numberBox.style.color = "black"
+    }
+    else if (currentNumber % 2 == 1) {
+        numberBox.style.color = "red";
+    }
+    else if (currentNumber % 2 == 0) {
+        numberBox.style.color = "green";
+    }
 })
